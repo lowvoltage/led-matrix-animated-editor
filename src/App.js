d9a48aef6d96b2121ce053d231cf624fae6dbc23
Dimitar Kovachev <mitko@blue-edge.bg>
2017-04-30 22:52:59 +0300

    CSS: Flow

--- src/App.js
+++ src/App.js
@@ -165,17 +165,16 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>8x8 LED Matrix Animated Editor</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <table className="led-matrix">
           <tbody>
             { rows }
           </tbody>
         </table>
-        { startStop } { addButton } { removeButton }
+        <div>
+          { startStop } { addButton } { removeButton }
+        </div>
         <div>
           Hex: { this.toHexString() }
         </div>
