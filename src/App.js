494ba69282c06718f0e1ceee8b74d4af5458c40a
Dimitar Kovachev <mitko@blue-edge.bg>
2017-04-30 11:34:05 +0300

    Enable SASS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import _ from 'lodash' 
+import _ from 'lodash';
 
 class LEDCell extends Component {
   onClick = () => {
@@ -9,7 +9,8 @@ class LEDCell extends Component {
   }
 
   render() {
-    let clazz = this.props.byte & (1 << this.props.col) ? "active" : null;
+    let clazz = "led-cell ";
+    clazz += this.props.byte & (1 << this.props.col) ? "active" : "";
     return <td className={clazz} onClick={this.onClick}> </td>;
   }
 }
@@ -17,8 +18,25 @@ class LEDCell extends Component {
 class LEDRow extends Component {
   render() {
     let cells = _.map(_.range(8), (c) => <LEDCell key={c} {...this.props} col={c} />);
+    return <tr>{ cells }</tr>;
+  }
+}
 
-    return (<tr> { cells } </tr>);
+class MatrixPreview extends Component {
+  componentDidMount() {
+      let ctx = this.canvas.getContext('2d');
+
+      ctx.fillStyle = 'rgb(200,0,0)';
+      ctx.fillRect(10, 10, 55, 50);
+  }
+
+  render() {
+    // let cells = _.map(_.range(8), (c) => <LEDCell key={c} {...this.props} col={c} />);
+    return (
+      <div style={{width: "200px", height: "200px", background: "blue"}}>
+        <canvas ref={(c) => { this.canvas = c; }} />
+      </div>
+    );
   }
 }
 
@@ -32,16 +50,23 @@ class App extends Component {
   }
 
   onLEDClick = (r, c) => {
-    // console.log(r, c);
     let newValues = _.clone(this.state.values);
     newValues[r] ^= (1 << c);
     this.setState({values: newValues});
   }
 
+  onHexChange = (e) => {
+    console.log(e);
+    console.log(e.target.value);
+  }
+
+  toHexString() {
+    let reversedValues = _.clone(this.state.values).reverse();
+    return _.map(reversedValues, (x) => ('0' + x.toString(16)).substr(-2)).join("");
+  }
+
   render() {
-    console.log(this.state.values);
     let rows = _.map(_.range(8), (r) => <LEDRow key={r} row={r} byte={this.state.values[r]} onClick={this.onLEDClick} />);
-    let stateVal = _.map(this.state.values, (x) => ('0' + x.toString(16)).substr(-2));
     return (
       <div className="App">
         <div className="App-header">
@@ -51,14 +76,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <table className="leds">
+        <table className="led-matrix">
           <tbody>
             { rows }
           </tbody>
         </table>
         <div>
-          { stateVal }
+          { this.toHexString() }
         </div>
+        Hex: <input type="text" defaultValue={this.toHexString()}  />
+        <MatrixPreview />
       </div>
     );
   }
